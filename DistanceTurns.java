1314cece35704831256d7d941e94cd092dacc55e
vigneshkumarp <47324662+vigneshkumarp@users.noreply.github.com>
2019-08-04 00:13:35 -0700

    Create DistanceTurns.java

---
+++ DistanceTurns.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Dist{
+public static void main(String[]args){
+Scanner Scan=new Scanner(System.in);
+
+
+int a=10;
+int b=20;
+int c=30;
+int d=40;
+int e=50;
+
+for(int n=2;n<=1000;n++){
+   if(n==3){
+           int f=a-c;
+          
+           
+          System.out.println("for "+n+"turns the meters  is:"+f );
+}
+if(n==4){
+int g= d-b;
+System.out.println("for "+n+" turns the meters is:"+g );
+}
+}
+}
+}
