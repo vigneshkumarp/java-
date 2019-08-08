b1c245cd84d52426c57a9e43f02823802bb8050e
vigneshkumarp <47324662+vigneshkumarp@users.noreply.github.com>
2019-08-08 13:37:15 +0530

    Create OddEvenInvalid.java

---
+++ OddEvenInvalid.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class OddEvenInvalid{
+public static void main(String[] arg){
+Scanner Scan=new Scanner(System.in);
+int a= Scan.nextInt();
+int b=0;
+if(a<b){
+System.out.println("invalid");
+}
+if((a % 2)==0){
+System.out.println("even");
+}
+else{
+System.out.print("odd");
+}
+
+}
+}
