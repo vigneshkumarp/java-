498a1edc5d1df48c7394f0236c3f5b147a36c55a
vigneshkumarp <47324662+vigneshkumarp@users.noreply.github.com>
2019-08-12 04:18:21 -0700

    Create MdigitSum.java

---
+++ MdigitSum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class MdigitSum{
+public static void main(String[]args){
+int a=445,b=567;
+int c,d,e,f,g;
+c=a/10;
+d=b/10;
+e=c%10;
+f=d%10;
+g=e+f;
+System.out.println("the sum is:"+g);
+}
+}
