11bd026936c81a4cd5eafc19298573aff7a2ffc5
vigneshkumarp <47324662+vigneshkumarp@users.noreply.github.com>
2019-08-12 04:15:25 -0700

    Create LdigitSum.java

---
+++ LdigitSum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class LdigitSum{
+public static void main(String[]args){
+int a=445,b=567;
+int c,d,e;
+c=a%10;
+d=b%10;
+e=c+d;
+System.out.println("the sum is:"+e);
+}
+}
