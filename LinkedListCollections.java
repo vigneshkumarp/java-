8ac675111ab60846ccfca404d0ef2aa5c1f9acca
vigneshkumarp <47324662+vigneshkumarp@users.noreply.github.com>
2019-08-19 14:19:06 +0530

    Create LinkedListCollections.java

---
+++ LinkedListCollections.java
@@ -0,0 +1,15 @@
+import java.util.*;  
+class LinkedListCollections{  
+ public static void main(String args[]){  
+  
+  LinkedList<String> al=new LinkedList<String>();  
+  al.add("vicky");  
+  al.add("Vijay");  
+  al.add("sadam");  
+  al.add("saran");  
+  
+  for(String obj:al){  
+   System.out.println(obj);  
+  }  
+ }  
+} 
