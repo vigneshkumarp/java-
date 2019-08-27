e1308940c7ee4ae574fb2b82808020d37393fdb1
vigneshkumarp <47324662+vigneshkumarp@users.noreply.github.com>
2019-08-27 07:15:32 -0700

    Create StringReverse1.java

---
+++ StringReverse1.java
@@ -0,0 +1,9 @@
+class StringReverse1{
+public static void main(String[]args){
+String s="hello java";
+StringBuilder x=new StringBuilder();
+x.append(s);
+x=x.reverse();
+System.out.println(x);
+}
+}
